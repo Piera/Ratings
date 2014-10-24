c858d572adae0395ebbda42cf7d9a2c31a7a92bc
Hackbright Student <info@hackbrightacademy.com>
2014-10-23 17:56:21 -0700

    Getting ready to populate database with seed users, movies, ratings

--- model.py
+++ model.py
@@ -1,13 +1,50 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker
+
+ENGINE = None
+Session = None
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key = True)
+    age = Column(Integer, nullable = True)
+    gender = Column(String(1), nullable = True)
+    occupation = Column(String(20), nullable = True)
+    zipcode = Column(String(15), nullable = True)
+
 ### Class declarations go here
+
+class Movies(Base):
+    __tablename__ = "movies"
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(150), nullable = False)
+    released_at = Column(DateTime, nullable = False)
+    imdb_url = Column(String(150), nullable = True)
+
+class Rating(Base):
+    __tablename__ = "ratings"
+    id = Column(Integer, primary_key = True)
+    movie_id = Column(Integer, nullable = False)
+    user_id = Column(Integer, nullable = False)
+    rating = Column(Integer, nullable = False)
 
 
 ### End class declarations
+
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo = True)
+    Session = sessionmaker(bind=ENGINE)
+
+    return Session()
 
 def main():
     """In case we need this for something"""
--- seed.py
+++ seed.py
@@ -1,9 +1,24 @@
 import model
 import csv
+import sys
 
 def load_users(session):
+
+    with open('u.user.csv') as csvfile:
+        user_file = csv.reader(csvfile, delimiter = '|')
+        for users in user_file:
+            id = (int(users[0]))
+            age = (int(users[1]))
+            gender = users[2]
+            occupation = users[3]
+            zipcode = (int(users[4]))
+            user = model.User(id= id.decode("latin-1"), age = age.decode("latin-1"), gender = gender.decode("latin-1"), occupation = occupation.decode(
+                "latin-1"), zipcode = zipcode.decode("latin-1"))
+            session.add(user)
+        session.commit()
+
     # use u.user
-    pass
+    
 
 def load_movies(session):
     # use u.item
@@ -14,9 +29,16 @@ def load_ratings(session):
     pass
 
 def main(session):
-    # You'll call each of the load_* functions with the session as an argument
-    pass
+    objectUsers,objectRatings,objectMovies = open(sys.argv[3])
+    load_users(objectUsers)
+    load_movies(objectMovies)
+    load_ratings(objectRatings)
 
+
+
+
+    # You'll all each of the load_* functions with the session as an argument
+    
 if __name__ == "__main__":
     s= model.connect()
     main(s)
