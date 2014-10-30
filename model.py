f4208275e5db974163ecd2ba5576a01132e36d27
Hackbright Student <info@hackbrightacademy.com>
2014-10-30 13:03:44 -0700

    Adding prediction functionalities

--- model.py
+++ model.py
@@ -27,6 +27,39 @@ class User(Base):
     username = Column(String(50), nullable = True)
     password = Column(String(50), nullable = True)
 
+    def similarity(self, other):
+        u_ratings = {}
+        paired_ratings =[]
+        for r in self.ratings:
+            u_ratings[r.movie_id] = r
+
+        for r in other.ratings:
+            u_r = u_ratings.get(r.movie_id)
+            if u_r:
+                paired_ratings.append( (u_r.rating, r.rating) )
+
+        if paired_ratings:
+            return correlation.pearson(paired_ratings)
+        else:
+            return 0.0
+
+    def predict(self, movie):
+        ratings = self.ratings
+        other_ratings = movie.ratings
+        other_users = [ r.user for r in other_ratings ]
+        similarities = [ (self.similarity(other_user), other_user) \
+            for other_user in other_users ]
+        similarities.sort(reverse = True)
+        top_user = similarities[0]
+        matched_rating = None
+        for rating in other_ratings:
+            if rating in other_ratings:
+                if rating.user_id == top_user[1].id:
+                    matched_rating = rating
+                    break
+        return matched_rating.rating * top_user[0]
+
+
 ### Class declarations go here
 
 class Movie(Base):
@@ -45,11 +78,11 @@ class Rating(Base):
     rating = Column(Integer, nullable = False)
 
     user = relationship("User", backref=backref("ratings", order_by=id))
+    # movie = relationship("Movie", backref=backref("ratings", order_by=id))
 
 def create_tables():
     global ENGINE
     Base.metadata.create_all(ENGINE)
-
 
 # def connect():
 #     global ENGINE
